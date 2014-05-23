bc7b441df247c3148133876a0c00af045265de9e
trycity <trycity@ca747c96-8bea-244d-9111-9a9389dd941b>
2014-05-23 19:18:33 +0000

    inserted doxygen tags for typedefs

--- trunk/utilities/GA_Typedefs.h
+++ trunk/utilities/GA_Typedefs.h
@@ -1,8 +1,14 @@
-//GA typedefs.h
+/**
+ * @file GA_Typedefs.h
+ * @author zshahid
+ *
+ */
 
 #include <vector>
 
+/// The typedef for a chromosome
 typedef std::vector<bool> Chromosome;
 
+/// The typedef for a population
 typedef std::vector<Chromosome> POPULATION;
 
