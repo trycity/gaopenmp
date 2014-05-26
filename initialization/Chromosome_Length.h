b2a473c3296465b7cac992046c672329d60bacd8
zubia861 <zubia861@ca747c96-8bea-244d-9111-9a9389dd941b>
2014-05-26 02:58:42 +0000

    new

---
+++ initialization/Chromosome_Length.h
@@ -0,0 +1,59 @@
+// class for Chromosome_Length
+/**
+ @file Chromosome_Length.h
+*/
+
+#ifndef CHROMOSOME_LENGTH_H
+#define CHROMOSOME_LENGTH_H
+
+#include <vector>
+#include <iterator> 
+
+#include "GA_Typedefs.h"
+
+class Chromosome_Length
+{
+public:
+
+   /// ctor
+   Chromosome_Length(unsigned int aPrecision, 
+                     unsigned int aNDIM, 
+                     std::vector<double>& adomain);
+
+   /// dtor
+   ~Chromosome_Length();
+
+   /// chromosome accessor
+   Chromosome getChromosome() const;
+
+   /// accessor
+   std::vector<unsigned int> getLength() const
+   {
+      return theLengths;
+   }
+  
+   /// build chromosome
+   void buildChromosome();
+   
+private:
+
+   unsigned int computeLength(double aLowerLimit, 
+                              double anUpperLimit);
+
+   unsigned int thePrecision;
+
+   unsigned int theNDIM;
+
+   std::vector<double>& domain;
+
+   Chromosome theChromosome; 
+
+   std::vector<unsigned int> theLengths;
+
+//#ifdef UNIT_TEST
+//   friend class Chromosome_Length_Test;
+//#endif
+   
+};
+
+#endif // CHROMOSOME_LENGTH_H
