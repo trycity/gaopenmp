74b2c5c7771b4b141380b5626d2768d453d75381
zubia861 <zubia861@ca747c96-8bea-244d-9111-9a9389dd941b>
2014-06-04 23:51:39 +0000

    initialization of chromosomes

---
+++ initialization/Population.h
@@ -0,0 +1,62 @@
+/**
+ *
+ */
+
+
+#ifndef POPULATION_HEADER
+#define POPULATION_HEADER
+
+//#include <bitset>
+
+#include "GA_Typedefs.h"
+
+
+/// the chromosome length
+// const unsigned int ChromosomeLength = 16;
+//typedef std::bitset<ChromosomeLength> Chromosome;
+
+//typedef std::vector<Chromosome> POPULATION;
+
+class Population
+{
+public:
+   
+   /**
+    * ctor to create a population object
+    *
+    * @param aSizeOfPopulation the size of the population
+    */
+   Population(unsigned int aSizeOfPopulation, 
+                       Chromosome aChromosome);
+
+   /**
+    * Destructor
+    */
+   ~Population();
+
+   /**
+    * const ref accessor method
+    *
+    * @return The population
+    */
+   /*const POPULATION& getPopulation() const;
+
+   
+    * Non-const accessor
+    *
+    * @return The population
+    */
+   POPULATION& getPopulation();
+	 void  Initialization();
+
+private:
+   /// Population size
+   unsigned int mSizeOfPopulation;
+
+   /// array of chromosomes
+   POPULATION mPopulation;
+};
+
+
+#endif // POPULATION_HEADER
+
