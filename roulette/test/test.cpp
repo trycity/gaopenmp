6c804ddf091f29bb90912ffc36fca53b5f7e16a2
zubia861 <zubia861@ca747c96-8bea-244d-9111-9a9389dd941b>
2014-06-25 01:54:42 +0000

    testing fitness

---
+++ roulette/test/test.cpp
@@ -0,0 +1,25 @@
+// test.cpp
+
+#include <iostream>
+#include "Fitness_test.h"
+
+
+
+
+
+int main()
+{
+   
+
+
+
+   std::cout<<"Begin roulette tests"<<std::endl<<std::endl;
+   
+   Fitness_test aTestObject;
+   aTestObject.runTests();
+   
+   std::cout<<"End of roulette tests"<<std::endl<<std::endl;	
+
+return 0;
+}
+
