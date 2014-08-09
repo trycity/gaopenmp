b98312ce0127f490967bfb52fd87866b2a719da6
trycity@gmail.com <trycity@gmail.com@ca747c96-8bea-244d-9111-9a9389dd941b>
2014-08-09 03:36:00 +0000

    white paper

--- trunk/roulette/test/Roulette_test.cpp
+++ trunk/roulette/test/Roulette_test.cpp
@@ -1,8 +1,7 @@
-
-
-
-
-
+#include <ctime> 
+#include <stdio.h>
+#include <ratio>
+#include <chrono>
 #include <vector>
 #include <iostream>
 
@@ -13,6 +12,8 @@
 #include "Roulette.h"
 #include "Population.h"
 #include "Fittness_value.h"
+
+using namespace std::chrono;
 
 Roulette_test::Roulette_test()
 {
@@ -55,13 +56,15 @@ void Roulette_test::testRoulette()
    unsigned int aPrecision = 4;
 
    std::vector<double> adomain;
+    for(int i=0; i<5; ++i)
+	{
    adomain.push_back(1.0);
    adomain.push_back(2.0);   
    adomain.push_back(3.0);
    adomain.push_back(6.0);
    adomain.push_back(8.0);
    adomain.push_back(16.0); 
-
+   }
    std::vector<unsigned int> length;
    unsigned int aNDIM = 3;
    unsigned int size = 10;
@@ -80,10 +83,19 @@ void Roulette_test::testRoulette()
     Function afunction;
     
    Roulette<Function> select( aPop, length, aNDIM, adomain, afunction);
+   high_resolution_clock::time_point t1 = high_resolution_clock::now();
+   
+   for(int i=0; i<10000; ++i)
+   {
    select.spinWheel();
-
+   }
+   high_resolution_clock::time_point t2 = high_resolution_clock::now();
+   
 	std::cout<<"Final population"<<std::endl;
 	printFarm(aPop.getPopulation());
+	duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+
+  std::cout << "It took me " << time_span.count() << " seconds."<<std::endl;
 }
 
 
