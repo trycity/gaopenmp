767a855088c735667f0f745ef77bf6bd45f1e69d
trycity <trycity@gmail.com>
2015-01-22 02:48:43 +0000

    inserted contents for class

---
+++ initialization/Configuration.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file Configuration.h
+ * @author trycity
+ */
+
+#include "Configuration.h"
+
+#include <omp.h>
+#include <vector>
+#include <random>
+
+Configuration::Configuration(unsigned int  aSizeOfPopulation, Chromosome aChromosome):
+							mSizeOfPopulation(aSizeOfPopulation)
+{
+   mPopulation.reserve(mSizeOfPopulation);
+   mPopulation.assign(mSizeOfPopulation, aChromosome);
+}
+
+Configuration::~Configuration()
+{
+   mPopulation.clear();
+}
+
+ void Configuration::Initialization()
+{
+   std::default_random_engine generator;
+	std::uniform_int_distribution<int> distribution(0,1);
+	unsigned int Chrom_size = mPopulation[0].size();
+
+	#pragma omp parallel for collapse(2)
+   for(unsigned int i =0; i< mSizeOfPopulation; i++)
+   {
+		for(unsigned int j=0; j<Chrom_size; j++)
+		{
+      	mPopulation[i][j]=static_cast<bool>(distribution(generator));
+		}
+	}
+}
+
+
+POPULATION& Configuration::getPopulation()
+{
+	return mPopulation;
+}
---
+++ initialization/Configuration.h
@@ -0,0 +1,58 @@
+/**
+ * @file Configuration.h
+ * @author trycity
+ */
+
+
+#ifndef CONFIGURATION_HEADER
+#define CONFIGURATION_HEADER
+
+
+#include "GA_Typedefs.h"
+
+
+class Configuration
+{
+public:
+
+   /**
+    * ctor to create an configuration for the population
+    *
+    * @param aSizeOfPopulation the size of the population
+    * @param aChromosome A chromosome of some particular size
+    */
+	Configuration(unsigned int aSizeOfPopulation,
+                       Chromosome aChromosome);
+
+   /**
+    * Destructor
+    */
+   ~Configuration();
+
+   /** Non-const accessor
+    *
+    * @return The population
+    */
+   POPULATION& getPopulation();
+
+   /**
+    * Create initial values for chromosomes
+    */
+   void  Initialization();
+
+private:
+   /// Population size
+   unsigned int mSizeOfPopulation;
+
+   /// array of chromosomes
+   POPULATION mPopulation;
+
+#ifdef UNIT_TEST
+   friend class Configuration_test;
+#endif //UNIT_TEST
+
+};
+
+
+#endif // CONFIGURATION_HEADER
+
