32e22fc7c031d05f6867c033a81a5d1b0743c3ed
trycity <trycity@gmail.com>
2015-01-23 01:04:07 +0000

    comment cleanup

--- initialization/Chromosome_Length.cpp
+++ initialization/Chromosome_Length.cpp
@@ -5,16 +5,15 @@
 
 #include <cmath>
 #include <omp.h>
+#include <iostream>
 
 #include "Chromosome_Length.h"
 
-
-Chromosome_Length::Chromosome_Length(unsigned int aPrecision, 
-                                     unsigned int aNDIM, 
-                                     const std::vector<double>& adomain):thePrecision(aPrecision), 
-                                     theNDIM(aNDIM), domain(adomain)
+Chromosome_Length::Chromosome_Length(unsigned int aPrecision,
+      unsigned int aNDIM, const std::vector<double>& adomain) :
+      thePrecision(aPrecision), theNDIM(aNDIM), domain(adomain)
 {
-   if(theNDIM != domain.size()/2)
+   if(theNDIM != domain.size() / 2)
    {
       throw;
    }
@@ -38,31 +37,31 @@ void Chromosome_Length::buildChromosome()
 {
    unsigned int chromosomeLength = 0;
    theLengths.resize(theNDIM, 0);
-   
+
    #pragma omp for ordered schedule(dynamic)
-   for(unsigned int i=0; i<theNDIM; ++i)
-   {      
-		unsigned int length = computeLength(domain[2*i], domain[2*i+1]);
-		
-		#pragma omp ordered
-		theLengths[i] = length;
-		
+   for(unsigned int i = 0; i < theNDIM; ++i)
+   {
+      unsigned int length = computeLength(domain[2 * i], domain[2 * i + 1]);
+      std::cout << "Number of threads = " << omp_get_num_threads() << std::endl;
+      #pragma omp ordered
+      theLengths[i] = length;
+
       chromosomeLength += length;
-	}
+   }
 
    theChromosome.resize(chromosomeLength);
 }
 
-unsigned int Chromosome_Length::computeLength(double aLowerLimit, 
-                                              double anUpperLimit)
+unsigned int Chromosome_Length::computeLength(double aLowerLimit,
+      double anUpperLimit)
 {
    unsigned int length = 0;
-   
-   double total = (anUpperLimit - aLowerLimit)*std::pow(10, thePrecision);
-   
-   while(total > std::pow(2, length)) length++;
-   
+
+   double total = (anUpperLimit - aLowerLimit) * std::pow(10, thePrecision);
+
+   while(total > std::pow(2, length))
+      length++;
+
    return length;
 }
-
 
